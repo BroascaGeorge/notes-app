b4f1e509f8f25a2a927266b5a65c4206792dee73
BroascaGeorge <georgebroasca98@gmail.com>
2023-06-14 11:52:32 +0300

    notes-component

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,5 +1,12 @@
-const Notes = () => {
-  return <div>Notes</div>;
+import * as React from "react";
+import { Note } from "../models/note.model";
+
+interface INotesProps {
+  note: Note;
+}
+
+const Notes: React.FunctionComponent<INotesProps> = ({ note }) => {
+  return <div>Note Card</div>;
 };
 
 export default Notes;
