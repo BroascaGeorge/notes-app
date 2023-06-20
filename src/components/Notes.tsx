abc2cbe0b4ea5dc7241ab708428c44da94a683c1
BroascaGeorge <georgebroasca98@gmail.com>
2023-06-20 14:29:57 +0300

    notes-comp

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,12 +1,27 @@
 import * as React from "react";
 import { Note } from "../models/note.model";
+import { Card } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 
 interface INotesProps {
   note: Note;
 }
 
-const Notes: React.FunctionComponent<INotesProps> = ({ note }) => {
-  return <div>Note Card</div>;
+const Notes: React.FC<INotesProps> = ({ note }) => {
+  return (
+    <div className="mb-3">
+      <Card>
+        <Card.Body>
+          <Card.Title>{note.title}</Card.Title>
+          <Card.Text>{note.text}</Card.Text>
+          <Card.Subtitle className="text-muted">{note.date}</Card.Subtitle>
+          <Button className="mt-3" variant="danger">
+            Delete
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default Notes;
