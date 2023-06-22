85939565d1214b79727c7be349bae22133b97fa1
BroascaGeorge <georgebroasca98@gmail.com>
2023-06-22 13:51:11 +0300

    delete-btn

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -5,17 +5,22 @@ import Button from "react-bootstrap/Button";
 
 interface INotesProps {
   note: Note;
+  handleDelete: (id: string) => void;
 }
 
-const Notes: React.FC<INotesProps> = ({ note }) => {
+const Notes: React.FC<INotesProps> = ({ note, handleDelete }) => {
   return (
     <div className="mb-3">
-      <Card>
+      <Card style={{ backgroundColor: note.color }}>
         <Card.Body>
           <Card.Title>{note.title}</Card.Title>
           <Card.Text>{note.text}</Card.Text>
           <Card.Subtitle className="text-muted">{note.date}</Card.Subtitle>
-          <Button className="mt-3" variant="danger">
+          <Button
+            className="mt-3"
+            variant="danger"
+            onClick={() => handleDelete(note.id)}
+          >
             Delete
           </Button>
         </Card.Body>
