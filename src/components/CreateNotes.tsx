9821ecccd98042462a58be1198550865d777bbb6
BroascaGeorge <georgebroasca98@gmail.com>
2023-06-26 14:23:35 +0300

    delete-btn

--- src/components/CreateNotes.tsx
+++ src/components/CreateNotes.tsx
@@ -1,5 +1,13 @@
-const CreateNotes = () => {
-  return <div>Create Notes</div>;
+import * as React from "react";
+
+interface ICreateNotesProps {}
+
+const CreateNotes: React.FunctionComponent<ICreateNotesProps> = (props) => {
+  return (
+    <>
+      <h2>Create Notes</h2>
+    </>
+  );
 };
 
 export default CreateNotes;
